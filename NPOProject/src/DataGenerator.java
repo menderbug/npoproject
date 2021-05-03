2fe72db2ee615382be751f540b4b99429f36dc5e
David Ye <dly8@case.edu>
2021-05-03 17:44:36 -0400

    more functionality like drop

--- NPOProject/src/DataGenerator.java
+++ NPOProject/src/DataGenerator.java
@@ -3,25 +3,38 @@ import java.nio.file.Paths;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DataGenerator {
 
-	enum Gender {MALE, FEMALE};
+	enum Gender {
+		MALE, FEMALE
+	};
 
-	//TODO generate unique id num (not UUID)
+	// TODO generate unique id num (not UUID)
+
+	class NPO {
+		int id;
+		String name;
+		String address;
+		String email;
+	}
 
 	class Person {
-
+		int id;
 		String fname, lname;
 		Gender gender;
 		String email;
 		String phone_num;
-		String address;		
-
+		String address;
+		int wealth;
 	}
 
 	private List<String> maleNames;
@@ -29,14 +42,15 @@ public class DataGenerator {
 	private List<String> lastNames;
 	private List<String> streets;
 	private List<String> suffixes = Arrays.asList("Road", "Lane", "Street", "Avenue", "Trail", "Place", "Way");
+	private List<String> domains = Arrays.asList("@gmail.com", "@yahoo.com", "@hotmail.com");
 
 	private static String server = "jdbc:sqlserver://npoproject.database.windows.net:1433;database=npoproject;user=azureuser@npoproject;password=6BILLIONturtles;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-	
+
 	public DataGenerator() {
-		try (Stream<String> mNames = Files.lines(Paths.get("male_names.txt"));
-				Stream<String> fNames = Files.lines(Paths.get("female_names.txt"));
-				Stream<String> lNames = Files.lines(Paths.get("last_names.txt"))
-				Stream<String> sts = Files.lines(Paths.get("streets.txt"))) {
+		try (Stream<String> mNames = Files.lines(Paths.get("src/male_names.txt"));
+				Stream<String> fNames = Files.lines(Paths.get("src/female_names.txt"));
+				Stream<String> lNames = Files.lines(Paths.get("src/last_names.txt"));
+				Stream<String> sts = Files.lines(Paths.get("src/streets.txt"))) {
 
 			maleNames = mNames.collect(Collectors.toList());
 			femaleNames = fNames.collect(Collectors.toList());
@@ -48,35 +62,71 @@ public class DataGenerator {
 		}
 
 	}
-	
 
 	public static void main(String[] args) {
-		
-		DataGenerator pt = new DataGenerator();
-		pt.populate();
 
+		DataGenerator pt = new DataGenerator();
+		pt.drop();
+		//pt.populate();
 	}
 
+	public void drop() {
+		List<String> tables = Arrays.asList("CurrentDate", "Department", "Donation", "Employee", "Expense", "Pledge", "Volunteer", "Donor", "Nonprofit");
+		try (Connection con = DriverManager.getConnection(server); Statement stmt = con.createStatement()) {
+			for (String s : tables)
+				stmt.executeUpdate("DROP TABLE IF EXISTS " + s);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void populate() {
 
 		try (Connection con = DriverManager.getConnection(server); Statement stmt = con.createStatement()) {
 
-			//Stream<>
+			// generating list of donors
+			List<Person> donors = generateHumans(300);
+			for (Person p : donors) {
+				p.email = (p.fname + "." + p.lname + randVal(domains)).toLowerCase();
+				stmt.executeUpdate(String.format(
+						"INSERT INTO Donor (donor_id, donor_name, donor_gender, donor_address, donor_email) VALUES ('%s', '%s', '%s', '%s', '%s')",
+						p.id, p.fname + " " + p.lname, p.gender, p.address, p.email));
+				for (int i = 0; i < rand(1, 5); i++) {
 
+				}
+				for (int i = 0; i < rand(1, 5); i++) {
 
-			String select = "SELECT * FROM CurrentDate";
-			
-			for (int i = 0; i < 1000; i++) {
-				stmt.executeUpdate(randInsert());
+				}
+
 			}
-			
-			// System.out.println(stmt.executeQuery(select));
 
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}	
+		}
 
+	}
+
+	private List<NPO> generateNPOs() {
+		List<String> npos = Arrays.asList("Estonian Red Cross", "Feeding Lebanon", "Ethan Greeley Institute",
+				"World Betterment", "Food for the Hungry", "San Francisco Zoo", "World Metro Opera",
+				"American Liver Fund", "Provide for Korea");
+		List<String> emails = Arrays.asList("info@erc.com", "info@feedinglebanon.com", "donate@egreeley.com",
+				"info@betterment.com", "contactus@ffth.com", "contact@sfzoo.com", "info@wmopera.com",
+				"contactus@alf.com", "donate@provide4korea.com");
+
+		List<NPO> output = new ArrayList<NPO>();
+
+		LinkedList<Integer> ids = new LinkedList<Integer>(randIDs(9));
+
+		for (int i = 0; i < 9; i++) {
+			NPO npo = new NPO();
+
+			npo.id = ids.poll();
+			npo.name = npos.get(i);
+			npo.email = emails.get(i);
+			npo.address = randAddress();
+		}
+		return output;
 	}
 
 	private List<Person> generateHumans(int num) {
@@ -94,14 +144,20 @@ public class DataGenerator {
 				p.gender = Gender.FEMALE;
 				p.fname = randVal(femaleNames);
 			}
-			
+
 			p.lname = randVal(lastNames);
 			p.phone_num = rand(101, 999) + "-" + rand(0, 999) + "-" + rand(0, 9999);
-			p.address = rand(1000, 99999) + " " + randVal(streets) + " " + randVal(suffixes); 
-			
+			p.address = randAddress();
+			p.wealth = rand(100, 100000);
+
 			output.add(p);
 
 		}
+
+		LinkedList<Integer> ids = new LinkedList<Integer>(randIDs(num));
+
+		for (Person p : output)
+			p.id = ids.poll();
 
 		return output;
 
@@ -109,6 +165,23 @@ public class DataGenerator {
 
 	// link expenses to departments and donors (maybe directors)
 	// track budget surplus/deficit
+
+	private Set<Integer> randIDs(int num) {
+		Set<Integer> output = new HashSet<Integer>();
+		while (output.size() < num) {
+			output.add(rand(10000, 99999));
+		}
+		return output;
+	}
+
+	private String randAddress() {
+		return rand(1000, 99999) + " " + randVal(streets) + " " + randVal(suffixes);
+	}
+
+	private Date randDate() {
+		long l = new GregorianCalendar(2021, 5, 3).getTimeInMillis() - new GregorianCalendar(2010, 1, 1).getTimeInMillis();
+		return new Date(new GregorianCalendar(2010, 1, 1).getTimeInMillis() + (long) (Math.random() * l));
+	}
 
 	private String randVal(List<String> list) {
 		return list.get((int) (Math.random() * list.size()));
@@ -118,11 +191,4 @@ public class DataGenerator {
 		return min + (int) (Math.random() * (max - min));
 	}
 
-	private String randInsert() {
-		return String.format("INSERT INTO CurrentDate (year, month, day) VALUES (%d, %d, %d)", rand(), rand(), rand());
-	}
-
-
-
-	
 }
--- NPOProject/src/create.sql
+++ NPOProject/src/create.sql
@@ -10,6 +10,7 @@ CREATE TABLE Donor
 (
 	donor_id VARCHAR(10) NOT NULL PRIMARY KEY,
 	donor_name VARCHAR(50) NOT NULL,
+	donor_gender VARCHAR(6) NOT NULL CHECK (donor_gender IN ('male', 'female')),
 	donor_address VARCHAR(50) NOT NULL,
 	donor_email VARCHAR(50) NOT NULL
 );
@@ -40,6 +41,7 @@ CREATE TABLE Employee
 	emp_id VARCHAR(10) PRIMARY KEY,
 	nonprofit_id VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES Nonprofit,
 	emp_name VARCHAR(50) NOT NULL,
+	emp_gender VARCHAR(6) NOT NULL CHECK (donor_gender IN ('male', 'female')),
 	phone_number numeric(11, 0) NOT NULL,
 	salary numeric(9, 2) NOT NULL
 );
@@ -72,6 +74,7 @@ CREATE TABLE Volunteer
 (
 	vol_id VARCHAR(10) NOT NULL PRIMARY KEY,
 	vol_name VARCHAR(50) NOT NULL,
+	vol_gender VARCHAR(6) NOT NULL CHECK (donor_gender IN ('male', 'female')),
 	field VARCHAR(50)
 );
 
